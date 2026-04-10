3be627e3dc97b364f2831720b2ff5ad19a71c8d8
dhhlu24 <tmp@example.com>
2026-04-11 10:51:37 +1200

    Add static.wixstatic.com to allowed image hosts

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'img.youtube.com' },
+      { protocol: 'https', hostname: '**.youtube.com' },
+      { protocol: 'https', hostname: '**.googleusercontent.com' },
+      { protocol: 'https', hostname: 'static.wixstatic.com' },
+    ],
   },
 };
 
